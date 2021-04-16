e19d2fa18ed70382eeee1fdd4000cf421b32c894
John Jesus <john.j.jesus@gmail.com>
2021-04-16 17:42:57 -0500

    correctly parsing XML

--- xml/main.cpp
+++ xml/main.cpp
@@ -14,6 +14,61 @@
 #include <string>
 
 
+
+void parse2(Poco::AutoPtr<Poco::XML::Document>& pDoc)
+{
+        Poco::XML::NodeIterator it(pDoc,
+            Poco::XML::NodeFilter::SHOW_ELEMENT
+        );
+        Poco::XML::Node* pNode = it.nextNode();
+        while (pNode)
+        {
+            if (pNode->nodeName() == "constant")
+            {
+                pNode = it.nextNode();
+                if (pNode->nodeName() == "name") {
+                    std::string text = pNode->innerText();
+                    std::cout << text << " ";
+                }
+                pNode = it.nextNode();
+                if (pNode->nodeName() == "value") {
+                    std::string text = pNode->innerText();
+                    std::cout << text << " ";
+                }
+                pNode = it.nextNode();
+                if (pNode->nodeName() == "comment") {
+                    std::string text = pNode->innerText();
+                    std::cout << text << " ";
+                }
+            }
+            std::cout << std::endl;
+            pNode = it.nextNode();
+        }
+}
+
+void parse1(Poco::AutoPtr<Poco::XML::Document>& pDoc)
+{
+        Poco::XML::NodeIterator it(pDoc,
+            Poco::XML::NodeFilter::SHOW_ALL
+        );
+        Poco::XML::Node* pNode = it.nextNode();
+        while (pNode)
+        {
+            if (pNode->nodeName() == "comment") {
+                pNode = it.nextNode();
+                if (pNode->nodeName() != "#text") {
+                    continue;
+                }
+                std::cout<< "Tag Text: " << pNode->nodeValue() << std::endl;
+            }
+            std::cout << pNode->nodeName() << ":" << pNode->nodeValue() <<
+std::endl;
+            std::cout << pNode->innerText() << std::endl;
+            pNode = it.nextNode();
+        }
+}
+
+
 int main(int argc, char** argv)
 {
     try {
@@ -22,16 +77,10 @@ int main(int argc, char** argv)
         Poco::XML::InputSource src(in);
 
         Poco::XML::DOMParser parser;
+        parser.setFeature(Poco::XML::DOMParser::FEATURE_FILTER_WHITESPACE, true);
         Poco::AutoPtr<Poco::XML::Document> pDoc = parser.parse(&src);
 
-        Poco::XML::NodeIterator it(pDoc, Poco::XML::NodeFilter::SHOW_ELEMENT);
-        Poco::XML::Node* pNode = it.nextNode();
-        while (pNode)
-        {
-            std::cout<<pNode->nodeName()<<":"<< pNode->nodeValue()<<std::endl;
-            pNode = it.nextNode();
-        }
-
+        parse2(pDoc);
     } catch (std::exception& e) {
         std::cout << e.what() << std::endl;
     }
